560994c561c7037e857bab71d655737c9d7d22ee
unknown <pasedasunady@gmail.com>
2024-02-08 10:53:03 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: "Poppins, sans-serif",
+      },
+      colors: {
+        "dark-heading": "#1f2428",
+        "dark-mode": "#191919",
+        "green-text": "#018C0F",
+     
+      },
+    },
+  },
+  plugins: [],
+};
